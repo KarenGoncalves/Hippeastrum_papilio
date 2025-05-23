e462d76a0e5960a534d85f4af183ec359d9e8070
KarenGoncalves <cris.kgs@gmail.com>
2025-05-23 15:01:07 -0400

    function to make fasta file be 2 lines per sequence only

---
+++ scripts/format_fasta.sh
@@ -0,0 +1,9 @@
+function format_fasta () {
+inFasta=$1
+outFasta=$2
+
+awk '!/^>/ { printf "%s", $0; n = "\n" }\
+ /^>/ { print n $0; n = "" }\
+ END { printf "%s", n }\
+ ' $inFasta  > $outFasta
+}
