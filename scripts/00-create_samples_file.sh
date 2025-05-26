555776f7a1b980a656e7ffb22bac17b1e23b1ac9
KarenGoncalves <cris.kgs@gmail.com>
2025-05-26 15:13:10 -0400

    creating samples_file from metadata.txt for assembly

--- scripts/00-create_samples_file.sh
+++ scripts/00-create_samples_file.sh
@@ -3,27 +3,13 @@
 DIR=$SCRATCH/Hippeastrum_papilio
 metadata=$DIR/metadata/metadata.txt
 
-cut -f 1 $metadata |\
- sed -E "s/([A-Z]{,2})([0-9]+)/\1\t\1\2/" |
+cut -f 1,2 $metadata |\
  awk -v DIR=$DIR 'BEGIN {OFS="\t"}\
- NR>1 {if ($1 == "R") {tissue="Root"}\
-	else if ($1 == "BS") {tissue="Basal_plate"}\
-	else if ($1 == "YF") {tissue="Young_leaf"}\
-	else if ($1 == "OF") {tissue="Old_leaf"}\
-	else if ($1 == "BI") {tissue="Inner_bulb"}\
-	else if ($1 == "PD") {tissue="Peduncle"}\
-	else if ($1 == "PI") {tissue="Pistil"}\
-	else if ($1 == "PT") {tissue="Petal"}\
-	else {tissue=$1};\
-	print tissue, $2, DIR"/"$2"_1.fastq", DIR"/"$2"_2.fastq"}'\
+	{print $2, $1, DIR"/"$1"_1.fastq", DIR"/"$1"_2.fastq"}'\
  > metadata/samples_file.txt
 
-# cut -f 1 means "select first column"
-# sed is using regex (-E) to get the sample name and the replicate number
-# the replacement (after the second /) creates one column for sample and one for replicate
+# cut -f 1,2 means "select first and second columns"
 # The awk command creates an awk variable for the directory,
 # indicates that the output field separator is a tab (BEGIN {}),
-# removes the column names (first row) from the data (NR > 1)
-# and then defines the actual tissue names based on the letters in column 1
-# Finally (after the ;) it indicates what is to be printed:
+# Finally it indicates what is to be printed:
 # SampleName	ReplicateName	Read1_path	Read2_path
