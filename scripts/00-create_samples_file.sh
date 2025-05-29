77e3a67aeebf035006f0920f0dbe2a63c8f743b7
Karen Cristine Gonçalves <cris.kgs@gmail.com>
2025-05-29 09:39:07 -0400

    Update 00-create_samples_file.sh

--- scripts/00-create_samples_file.sh
+++ scripts/00-create_samples_file.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-DIR=$SCRATCH/Hippeastrum_papilio/clean_reads
-metadata=$DIR/../metadata/metadata.txt
+DIR=$SCRATCH/Hippeastrum_papilio
+metadata=$DIR/metadata/metadata.txt
 
 cut -f 1,2 $metadata |\
  awk -v DIR=$DIR 'BEGIN {OFS="\t"}\
-	{print $2, $1, DIR"/"$1"_1.fastq", DIR"/"$1"_2.fastq"}'\
- > $DIR/../metadata/samples_file.txt
+	{print $2, $1, DIR"/clean_reads/"$1"_1.fastq", DIR"/clean_reads/"$1"_2.fastq"}'\
+ > $DIR/metadata/samples_file.txt
 
 # cut -f 1,2 means "select first and second columns"
 # The awk command creates an awk variable for the directory,
