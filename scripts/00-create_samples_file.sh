2ac2e7cadeb57d4af80d6c60943093830c7b1852
sajjad992 <sajjad324@gmail.com>
2025-05-29 12:48:31 -0400

    corrected samples file

--- scripts/00-create_samples_file.sh
+++ scripts/00-create_samples_file.sh
@@ -3,7 +3,7 @@
 DIR=$SCRATCH/Hippeastrum_papilio
 metadata=$DIR/metadata/metadata.txt
 
-cut -f 1,2 $metadata |\
+cut -d ' ' -f 1,2 $metadata |\
  awk -v DIR=$DIR 'BEGIN {OFS="\t"}\
 	{print $2, $1, DIR"/clean_reads/"$1"_1.fastq", DIR"/clean_reads/"$1"_2.fastq"}'\
  > $DIR/metadata/samples_file.txt
