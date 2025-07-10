510bc20e501a5f8a56a21dc233f7b5366163d521
Karen Cristine Gonçalves <cris.kgs@gmail.com>
2025-07-10 12:05:46 -0400

    Update create_busco_table.sh

--- scripts/create_busco_table.sh
+++ scripts/create_busco_table.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-echo "Database liliopsida_odb10
-Species Hippeastrum-papilio" | tr " " "\t" | tr "-" " " > table_busco_result
+## Database liliopsida_odb10
+## Species Hippeastrum-papilio
+echo "Category Value" | tr " " "\t" | tr "-" " " > table_busco_result
 
 grep 'one_line_summary' busco/short_summary.specific.liliopsida_odb10.busco.json\
  | sed -E 's/^ +//' | cut -f 2 -d ' ' |\
 sed -E 's/"C:([0-9+\.]+)%\[S:([0-9\.]+)%,D:([0-9\.]+)%\],F:([0-9\.]+)%,M:([0-9\.]+)%,n:([0-9]+)",/Complete \1\nSingle \2\nDuplicated \3\nFragmented \4\nMissing \5\nn_markers \6/'\
- | tr " " "\t" >> table_busco_result
+ | tr " " "\t" | grep -v "Complete" >> table_busco_result
