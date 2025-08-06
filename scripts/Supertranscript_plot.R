7ca3f14ae513938f5a7403b3e50f6d00a4ca1af2
KarenGoncalves <cris.kgs@gmail.com>
2025-08-06 15:05:05 -0400

    supertranscripts

--- scripts/Supertranscript_plot.R
+++ scripts/Supertranscript_plot.R
@@ -1,14 +1,16 @@
 library(tidyverse)
+library(devtools)
 # devtools::install_github("dzhang32/ggtranscript")
 library(ggtranscript)
+# install.packages(c("rtracklayer", "cowplot"))
 library(rtracklayer)
 library(cowplot)
 
-gtf <- rtracklayer::import("D:/Work/Hpapilio_2025/trinity_genes.gtf") %>% 
+gtf <- import("D:/Work/Hpapilio_2025/trinity_genes.gtf") %>% # function from rtracklayer)
     as_tibble
 
 genes = paste0("Hipap_TRINITY_",
-               c("DN7708_c0", "DN88533_c1", "DN16473_c1"),
+               c("DN7708_c0", "DN88533_c1", "DN16473_c1"), # add the gene id's of interest
                "_g1"
 )
 
@@ -20,10 +22,14 @@ rel_heights =
     group_by(gene_id) %>%
     count %>%
     mutate(gene_id = factor(gene_id, levels=genes)) %>%
-        arrange(gene_id))$n
+        arrange(gene_id))$n # this is for cowplot, to give space in the plot relative to the number of isoforms of each gene
 
-# isoform_lengths = 
-        
+isoform_lengths = 
+    (gtf %>%
+    filter(gene_id %in% genes) %>%
+    group_by(gene_id, transcript_id) %>%
+    summarize(Transcript_width = sum(width))
+)
 
 sapply(genes, simplify = F, \(x) {
     gtf %>%
